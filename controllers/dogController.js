167f52b0afe8f61e940d46c9551a0c9a0eec21dd
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-04-15 23:45:31 -0500

    all set

--- controllers/dogController.js
+++ controllers/dogController.js
@@ -56,126 +56,35 @@ router.delete('/:id', (req, res, next) => {
         })
     })
 
-// get  index
-// router.get('/', async (req, res, next) => {
-//   try {
-//     const dogsWithOwners = await Dog.find({}).populate('dogs')
-//     console.log("\nfound dogs")
-//     console.log(foundDogs);
-//     res.render('dogs/index.ejs', {
-//       dogs: foundDogs
-//     })
-//   } catch(err) {
-//     next(err)
-//   }
-// })
 
-// router.get('/new', (req, res) => {
-//     res.render('dogs/new.ejs')
-// })
+router.get('/:id/edit', (req, res, next) => {
+  Dog.findById(req.params.id, (err, foundDog) => {
+    if(err) next(err);
+    else {
+      Owner.find({}, (err2, foundOwners) => {
+        if(err2) next(err2);
+        else {
+          res.render('dogs/edit.ejs', {
+            dog: foundDog,
+            owners: foundOwners
+          })
+        }        
+      })
+    }
+  })
+})
 
-// router.get('/:id', async (req, res, next) => {
-//     try {
-      
-//         const foundDog = await Dog.findById(req.params.id)
-//         const foundOwners = await Owner.find({
-//             dog: req.params.id
-//         })
-//         res.render('dogs/show.ejs', {
-//             dog: foundDog,
-//             owners: foundOwners
-//         })
-//     } catch (err) {
-//         next(err)
-//     }
-// })
-
-// router.post('/', async (req, res, next) => {
-//     try {
-        
-//         const createdDog = await Dog.create(req.body)
-//         console.log("dogogogogogogoogogog");
-//         res.redirect('/dogs')
-//     } catch (err) {
-//         next(err)
-//     }
-// })
-
-// router.delete('/:id', async (req, res, next) => {
-//     try {
-
-//         const ownerDeleteError = await owner.remove({
-//             dog: req.params.id
-//         })
-//         const deletedDog = await Dog.findByIdAndRemove(req.params.id)
-//         res.redirect('/dogs')
-//     } catch (err) {
-//         next(err)
-//     }
-// })
-
-// router.get('/:id/edit', async (req, res, next) => {
-//     try {
-
-//         const foundDog = await Dog.findById(req.params.id)
-//         res.render('dogs/edit.ejs', {
-//             dog: foundDog
-//         })
-//     } catch(err) {
-//         next(err)
-//     }
-// })
-
-// router.put('/:id', async (req, res, next) => {
-//     try {
-//         const updatedDog = await Dog.findByIdAndUpdate(req.params.id, req.body, {
-//             new: true
-//         })
-//         res.redirect(`/dogs/${updatedDog._id}`)
-//     } catch (err) {
-//         next(err)
-//     }
-// })
+router.put('/:id', (req, res, next) => {
+  Dog.findByIdAndUpdate(req.params.id, req.body, (err, updatedDog) => {
+    if(err) next(err);
+    else {
+      // redirect back to article show page so user can see updates
+      res.redirect('/dogs/' + updatedDog._id)
+    }    
+  })
+})
 
 
-
-// // router.get('/', (req, res, next) => {
-// //     Dog.find({}, (err, foundDog) => {
-// //         if (err) {
-// //             next(err)
-// //         } else {
-// //             res.render('home.ejs')
-// //         }
-// //     })
-
-// // })
-
-// // router.post('/', (req, res, next) => {
-// //   console.log("\nhere is req.body -- it was added by body-parser");
-// //   console.dir(req.body);
-
-// //   const dogToAdd = {
-// //     name: req.body.name,
-// //     mood: req.body.mood,
-// //     cuteness: req.body.cuteness,
-// //     hungry: req.body.hungry,
-// //     scratchesNeed: req.body.scratchesNeed
-// //   }
-
-// //   req.body.hungry = "on"
-// //   req.body.scratchesNeed = "on"
-
-// //   Dog.create(dogToAdd, (err, createdDog) => {
-// //       if (err) {
-// //           next(err)
-// //       } else {
-// //           console.log("\n here is the dog we created");
-// //           console.log(createdDog);
-// //           res.redirect('/dog')
-// //           // res.send('hitting create route')
-// //       }
-// //   })
-// // })
 
 module.exports = router
 
--- controllers/ownerController.js
+++ controllers/ownerController.js
@@ -57,10 +57,15 @@ router.post('/', (req, res, next) => {
 
 // destroy
 router.delete('/:id', (req, res, next) => {
-    Owner.findByIdAndRemove(req.params.id, (err, deletedOwner) => {
-        if(err) nest(err)
-            else{
-                res.redirect('/owners')
+    Owner.remove({ owner: req.params.id }, (dogDeleteError, result) => {
+        if(dogDeleteError) next(dogDeleteError)
+            else {
+                Owner.findByIdAndRemove(req.params.id, (err, deletedOwner) => {
+                    if(err) next(err)
+                        else {
+                            res.redirect('/owners')
+                        }
+                })
             }
     })
 })
@@ -93,97 +98,6 @@ router.put('/:id', (req, res, next) => {
         }
     )
 })
-
-// article index route: GET /articles
-// router.get('/', async (req, res, next) => {
-//     try {
-//         const foundOwners = await Owner.find({}).populate('dog')
-//         res.render('owners/index.ejs', {
-//             owners: foundOwners
-//         })
-//     } catch (err) {
-//         next(err)
-//     }
-
-// }) // index route
-
-// // DONE
-// // article new route: GET /articles/new
-// router.get('/new', async (req, res, next) => {
-//     try {
-//         const foundDogs = await Dog.find({}).populate('owner')
-//         res.render('owners/new.ejs', {
-//             dogs: foundDogs
-//         })
-//     } catch (err) {
-//         next(err)
-//     }
-// })
-
-
-
-// // DONE
-// // article show route: GET /articles/:id
-// router.get('/:id', async (req, res, next) => {
-//     try {
-//         // get article with author populated
-//         const foundArticle = await Article.findById(req.params.id).populate('author')
-//         // render template
-//         res.render('articles/show.ejs', {
-//             article: foundArticle
-//         })
-//     } catch (error) {
-//         next(error)
-//     }
-
-// }) // article show: GET /articles/:id
-
-
-// // DONE
-// // article create route: POST /articles
-// router.post('/', async (req, res, next) => {
-//     try {
-//         const createdOwner = await Owner.create(req.body)
-//         res.redirect('/owners')
-//     } catch (err) {
-//         next(err)
-//     }
-// })
-
-// // article destroy route: DELETE /articles/:id
-// router.delete('/:id', async (req, res, next) => {
-//     try {
-//         const deletedOwner = await Owner.findByIdAndRemove(req.params.id)
-//         res.redirect('/dogs/' + deletedOwner.dog)
-//     } catch (err) {
-//         next(err)
-//     }
-// })
-
-// router.get('/:id/edit', async (req, res, next) => {
-//     try {
-//         const foundOwner = await Owner.findById(req.params.id)
-//         const foundDogs = await Dog.find({}).populate('dogs')
-//         res.render('owners/edit.ejs', {
-//             owner: foundOwners,
-//             dogs: foundDogs
-//         })
-//     } catch (err) {
-//         next(err)
-//     }
-// })
-
-
-// // DONE
-// // article update route: PUT /articles/:id
-// router.put('/:id', async (req, res, next) => {
-//     try {
-//         const updatedOwner = await Owner.findByIdAndUpdate(req.params.id, req.body)
-//         res.redirect('/owners/' + updatedOwner._id)
-//     } catch (err) {
-//         next(err)
-//     }
-// })
 
 
 module.exports = router
